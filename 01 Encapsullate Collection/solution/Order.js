b8525538d4fad8ea2b89417637c13a08298b31c8
tomas.corral <7e16f4105658d7d343b2c6000f03d8b7>
2013-01-17 10:25:32 +0100

    Update solution 1 to take advantage of object prototype.

--- 01 Encapsullate Collection/solution/Order.js
+++ 01 Encapsullate Collection/solution/Order.js
@@ -1,23 +1,31 @@
+// This solution takes advantage of prototype Thanks RoryH
 function Order() {
-    var aOrderLines = [];
-    this.nOrderTotal = 0;
-
-    this.getOrderLines = function () {
-        return aOrderLines.concat();
-    };
-    this.addOrderLine = function (oOrderLine) {
-        this.nOrderTotal += oOrderLine.nTotal;
-        aOrderLines.push(oOrderLine);
-    };
-    this.removeOrderLine = function (oOrderLineItem) {
-        var oOrderLine;
-        oOrderLine = aOrderLines.map(function (oOrder) {
-            return oOrder === oOrderLineItem;
-        })[0];
-        if (typeof oOrderLine === 'undefined' || oOrderLine === null) {
-            return;
-        }
-        this.nOrderTotal -= oOrderLine.nTotal;
-        aOrderLines.splice(this.nOrderTotal, 1);
-    };
+	this.nOrderTotal = 0;
 }
+Order.prototype = (function()
+{
+	var aOrderLines = [];
+	return {
+		getOrderLines: function()
+		{
+			return aOrderLines.concat();
+		},
+		addOrderLine: function()
+		{
+			this.nOrderTotal += oOrderLine.nTotal;
+			aOrderLines.push(oOrderLine);
+		},
+		removeOrderLine: function()
+		{
+			var oOrderLine;
+			oOrderLine = aOrderLines.map(function (oOrder) {
+				return oOrder === oOrderLineItem;
+			})[0];
+			if (typeof oOrderLine === 'undefined' || oOrderLine === null) {
+				return;
+			}
+			this.nOrderTotal -= oOrderLine.nTotal;
+			aOrderLines.splice(this.nOrderTotal, 1);
+		}
+	};
+}());
