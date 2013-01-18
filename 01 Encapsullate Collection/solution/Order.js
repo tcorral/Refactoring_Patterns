968f95dc88bb7be08f1f8f3d7d15fac69deb7d2c
tomas.corral <7e16f4105658d7d343b2c6000f03d8b7>
2013-01-18 13:24:01 +0100

    Fix error while taking advantage of prototype.

--- 01 Encapsullate Collection/solution/Order.js
+++ 01 Encapsullate Collection/solution/Order.js
@@ -1,31 +1,42 @@
 // This solution takes advantage of prototype Thanks RoryH
 function Order() {
+	this.nId = Unique.getKey();	// Unique.getKey is an static method to get an unique key for instance.
 	this.nOrderTotal = 0;
 }
 Order.prototype = (function()
 {
-	var aOrderLines = [];
+	var oOrderLines = {};
+	var setOrderLinesByInstance = function(oInstance)
+	{
+		if(typeof oOrderLines[oInstance.nId] === 'undefined')
+		{
+			oOrderLines[oInstance.nId] = [];
+		}
+	};
 	return {
 		getOrderLines: function()
 		{
-			return aOrderLines.concat();
+			setOrderLinesByInstance(this);
+			return oOrderLines[this.nId].concat();
 		},
 		addOrderLine: function(oOrderLine)
 		{
+			setOrderLinesByInstance(this);
 			this.nOrderTotal += oOrderLine.nTotal;
-			aOrderLines.push(oOrderLine);
+			oOrderLines[this.nId].push(oOrderLine);
 		},
 		removeOrderLine: function(oOrderLineItem)
 		{
 			var oOrderLine;
-			oOrderLine = aOrderLines.map(function (oOrder) {
+			setOrderLinesByInstance(this);
+			oOrderLine = oOrderLines[this.nId].map(function (oOrder) {
 				return oOrder === oOrderLineItem;
 			})[0];
 			if (typeof oOrderLine === 'undefined' || oOrderLine === null) {
 				return;
 			}
 			this.nOrderTotal -= oOrderLine.nTotal;
-			aOrderLines.splice(this.nOrderTotal, 1);
+			oOrderLines[this.nId].splice(this.nOrderTotal, 1);
 		}
 	};
 }());
