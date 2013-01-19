6762c4494f80418bb268e31d0747cad193066b3f
tcorral <amischol@gmail.com>
2013-01-19 15:57:20 +0100

    Adding a mix of taking advantage of prototype but maintain private aOrderLines without hacking job of Garbage Collector.

---
+++ 01 Encapsullate Collection/solution_using_prototype/Order2.js
@@ -0,0 +1,37 @@
+function Order2()
+{
+    var aOrderLines = [];
+    this.getOrderLines = function()
+    {
+        return aOrderLines.concat();
+    };
+    this.setOrderLines = function(aOrder)
+    {
+        var aToConcat = aOrder || [];
+        aOrderLines = aOrderLines.concat(aToConcat);
+        aToConcat = null;
+    };
+}
+Order2.prototype.addOrderLine = function (oOrderLine)
+{
+    var aOrderLines = [];
+    this.nOrderTotal += oOrderLine.nTotal;
+    aOrderLines.push(oOrderLine);
+    this.setOrderLines(aOrderLines);
+    aOrderLines = null;
+};
+Order2.prototype.removeOrderLine = function(oOrderLineItem)
+{
+    var aOrderLines = this.getOrderLines(),
+        oOrderLine;
+    oOrderLine = aOrderLines.map(function (oOrder) {
+        return oOrder === oOrderLineItem;
+    })[0];
+    if (typeof oOrderLine === 'undefined' || oOrderLine === null) {
+        return;
+    }
+    this.nOrderTotal -= oOrderLine.nTotal;
+    aOrderLines.splice(this.nOrderTotal, 1);
+    this.setOrderLines(aOrderLines);
+    oOrderLine = aOrderLines = null;
+};
